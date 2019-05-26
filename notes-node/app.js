8a3ccdf4f9f29d62ab76650c25f5f4c404006acf
chandan1602 <cbansal82@gmail.com>
2019-05-26 22:19:15 +0530

    Till yargs

--- notes-node/app.js
+++ notes-node/app.js
@@ -1,28 +1,52 @@
 console.log('Starting app.js');
 
+// const fs = require('fs');
+// const os = require('os');
+// const _=require('lodash');
+// const notes = require('./notes.js');
+//
+// var user = os.userInfo();
+// console.log(user);
+//
+// // var res = notes.addNote();
+// // console.log(res);
+//
+// // fs.appendFile('Greetings.txt', 'Hello ${user.username}! You are ' + notes.age, function(err) {
+// // if (err) {
+// //   console.log('Unable to open file');
+// // }
+// // });
+//
+// var total = notes.add(2,3);
+// console.log(total);
+//
+//
+// console.log(_.isString(true));
+// console.log(_.isString('Chandan'));
+//
+// var filteredArray = _.uniq(['Chandan', 1, 'Chandan', 1, 2, 3, 4, 5]);
+// console.log(filteredArray);
+
 const fs = require('fs');
-const os = require('os');
 const _=require('lodash');
+const yargs = require('yargs');
+
 const notes = require('./notes.js');
 
-var user = os.userInfo();
-console.log(user);
+const argv = yargs.argv;
+var command = process.argv[2];
+console.log('Command: ', command);
+console.log('Process', process.argv);
+console.log('Yargs', argv);
 
-// var res = notes.addNote();
-// console.log(res);
-
-// fs.appendFile('Greetings.txt', 'Hello ${user.username}! You are ' + notes.age, function(err) {
-// if (err) {
-//   console.log('Unable to open file');
-// }
-// });
-
-var total = notes.add(2,3);
-console.log(total);
-
-
-console.log(_.isString(true));
-console.log(_.isString('Chandan'));
-
-var filteredArray = _.uniq(['Chandan', 1, 'Chandan', 1, 2, 3, 4]);
-console.log(filteredArray);
+if (command === 'add') {
+  notes.addNote(argv.title, argv.body);
+} else if (command === 'list'){
+  notes.getAll();
+} else if (command === 'read'){
+  notes.getNote(argv.title);
+} else if (command === 'remove'){
+  notes.removeNote(argv.title);
+} else {
+  console.log('Not recognised');
+}
--- notes-node/notes.js
+++ notes-node/notes.js
@@ -5,7 +5,30 @@ console.log('Starting notes.js');
 //   return 'New Note';
 // };
 
-module.exports.add = (a, b) => {
-  var c = a + b;
-  return c;
-}
+// module.exports.add = (a, b) => {
+//   var c = a + b;
+//   return c;
+// }
+
+var addNote = (title, body) => {
+  console.log('Adding note', title, body);
+};
+
+var getAll = () => {
+  console.log('Getting all notes');
+};
+
+var getNote = (title) => {
+  console.log('Reading note : ', title);
+};
+
+var removeNote = (title) => {
+  console.log('Removing note : ', title);
+};
+
+module.exports = {
+  addNote: addNote,
+  getAll: getAll,
+  getNote: getNote,
+  removeNote: removeNote
+};
