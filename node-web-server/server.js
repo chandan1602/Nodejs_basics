71a46002199c0fd76e42c511f5b1e6620d3e87ce
chandan1602 <cbansal82@gmail.com>
2019-05-28 02:07:51 +0530

    Building Web Server- Done upto Partials

---
+++ node-web-server/server.js
@@ -0,0 +1,38 @@
+const express = require('express');
+const hbs = require('hbs');
+
+var app = express();
+
+hbs.registerPartials(__dirname + '/views/partials');
+app.set('view engine', 'hbs');
+app.use(express.static(__dirname + '/public'));
+
+hbs.registerHelper('getCurrentYear', () => new Date().getfullYear());
+
+hbs.registerHelper('sreamIt', (text) => {
+  return text.toUpperCase();
+});
+
+app.get('/', (req, res) => {
+  res.render('home.hbs', {
+    pageTitle: 'HomePage',
+    WelcomeMessage: 'Welcome to HomePage'
+  });
+});
+
+app.get('/about', (req, res) => {
+  res.render('about.hbs', {
+    pageTitle: 'About Page',
+  });
+});
+
+app.get('/bad', (req, res) => {
+  res.send({
+    title: 'Error 404',
+    Description: 'Please check your Internet Connection'
+  });
+});
+
+app.listen(3000, () => {
+  console.log('Server is up on port 3000');
+});
