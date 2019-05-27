1dc790705013d65a82a4966780c554042b9d587d
chandan1602 <cbansal82@gmail.com>
2019-05-27 11:51:14 +0530

    Done upto addNote function

--- notes-node/notes.js
+++ notes-node/notes.js
@@ -10,9 +10,37 @@ console.log('Starting notes.js');
 //   return c;
 // }
 
+const fs = require('fs');
+
 var addNote = (title, body) => {
-  console.log('Adding note', title, body);
+  var notes = [];
+  var note = {
+    title: title,
+    body: body
+  };
+
+  try {
+    var notesString = fs.readFileSync('notes-data.json');
+    notes = JSON.parse(notesString);
+
+  }catch (e) {
+
+  }
+
+
+var duplicateNotes = notes.filter((note) => note.title === title);
+
+if (duplicateNotes.length === 0) {
+  notes.push(note);
+  fs.writeFileSync('notes-data.json', JSON.stringify(notes));
+
+}
 };
+// var duplicateNotes = motes.filter((note) => {
+//   return note.title === title;
+// });
+
+
 
 var getAll = () => {
   console.log('Getting all notes');
