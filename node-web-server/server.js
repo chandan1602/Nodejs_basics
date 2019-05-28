8e7d172d0bf081acce709ae7b0bbeac2b612e448
chandan1602 <cbansal82@gmail.com>
2019-05-28 17:24:45 +0530

    Upto heroku

--- node-web-server/server.js
+++ node-web-server/server.js
@@ -1,17 +1,35 @@
 const express = require('express');
 const hbs = require('hbs');
-
+const fs = require('fs');
+const port = process.env.PORT || 3000;
 var app = express();
-
 hbs.registerPartials(__dirname + '/views/partials');
 app.set('view engine', 'hbs');
+
+app.use((req, res, next) => {
+  var now = new Date().toString();
+  var log = '${now}: ${req.method} ${req.originalUrl}';
+  console.log(now, req.method, req.originalUrl);
+  fs.appendFile('Server.log', log + '\n', (err) => {
+    if(err) {
+      console.log('Unable to append to server.log');
+    }
+  });
+  next();
+});
+
+app.use((req, res, next) => {
+res.render('maintanance.hbs');
+});
+
 app.use(express.static(__dirname + '/public'));
 
-hbs.registerHelper('getCurrentYear', () => new Date().getfullYear());
-
-hbs.registerHelper('sreamIt', (text) => {
-  return text.toUpperCase();
+hbs.registerHelper('getCurrentYear', () => {
+  return new Date().getFullYear();
 });
+// hbs.registerHelper('screamIt', (text) => {
+//   return toUpperCase(text);
+// });
 
 app.get('/', (req, res) => {
   res.render('home.hbs', {
@@ -33,6 +51,6 @@ app.get('/bad', (req, res) => {
   });
 });
 
-app.listen(3000, () => {
-  console.log('Server is up on port 3000');
+app.listen(port, () => {
+  console.log('Server is up on port', port);
 });
