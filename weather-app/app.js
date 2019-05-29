f6c8e95484b66bfef2d5c0c10020d16fbe376aca
chandan1602 <cbansal82@gmail.com>
2019-05-29 23:16:15 +0530

    Error showing weather results but in address, no error

--- weather-app/app.js
+++ weather-app/app.js
@@ -1,5 +1,7 @@
-const request = require('request');
 const yargs = require('yargs');
+
+const geocode = require('./geocode/geocode');
+const weather = require('./weather/weather');
 
 const argv = yargs
 .options({
@@ -14,24 +16,21 @@ a: {
 .alias('help', 'h')
 .argv;
 
-console.log(argv);
+// console.log(argv);
 
-// var address =  argv
-var encodedAddress = encodeURIComponent(argv.address);
-
-request({
-  url: `https://maps.googleapis.com/maps/api/geocode/json?address=${encodedAddress}&key=AIzaSyArue3mB2ifINLkZhumlEqjZEvn9YVzGa4`,
-  json: true
-}, (error, response, body) => {
-if(error) {
-  console.log('Unable to connect to google servers!');
-} else if(body.status === 'ZERO_RESULTS') {
-  console.log('Unable to find full address!');
-} else if (body.status === 'OK') {
-
-  // console.log(JSON.stringify(response, undefined, 2));
-  console.log('Address', body.results[0].formatted_address)
-  console.log('Latitude', body.results[0].geometry.location.lat )
-  console.log('Longitude', body.results[0].geometry.location.lng )
-}
+geocode.geocodeAddress(argv.address, (errorMessage, results) => {
+  if (errorMessage) {
+    console.log(errorMessage);
+  } else {
+    console.log(results.address);
+    weather.getWeather(results.latitude, results.longitude, (errorMessage, weatherResults) => {
+      if (errorMessage) {
+        console.log(errorMessage);
+      } else {
+        console.log(`It's currently ${weatherResults.temperature}. It feels like ${weatherResults.apparentTemperature}.`);
+      }
+    });
+  }
 });
+
+// lat,lng,callback
