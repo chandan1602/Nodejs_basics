20c5bc2635026c031b0b94f00a3982b8c2717e1a
chandan1602 <cbansal82@gmail.com>
2019-06-06 16:38:23 +0530

    Done till postman

--- -todo-api/server/server.js
+++ -todo-api/server/server.js
@@ -1,23 +1,28 @@
-var mongoose = require('mongoose');
+var express = require('express');
+var bodyParser = require('body-parser');
 
-mongoose.Promise = global.Promise;
-mongoose.connect('mongodb://localhost:27017/TOdoApp');
+var {mongoose} = require('./db/mongoose.js')
+var {Todo} = require('./models/todo')
+var {User} = require('./models/user')
 
-var Todo = mongoose.model('Todo', {
-  text: {
-    type: String,
-    required: true,
-    minlength: 1,
-    trim: true
-  },
-  completed: {
-    type: Boolean,
-    default: false
-  },
-  completedAt: {
-    type: Number,
-    default: null
-  }
+var app = express();
+
+app.use(bodyParser.json());     //Middleware
+
+app.post('/todos', (req, res) => {
+  var todo = new Todo({
+    text: req.body.text
+  });
+
+  todo.save().then((doc) => {
+    res.send(doc);
+  }, (e) => {
+    res.status(400).send(e);
+  })
+});
+
+app.listen(3000, () => {
+  console.log('Started on Port 3000');
 });
 
 // var newTodo = new Todo({
@@ -43,24 +48,12 @@ var Todo = mongoose.model('Todo', {
 //   console.log('Unable to save Todo');
 // });
 
-//User=>
-//email-require it - trim it - set type - set min length of 1
-
-var User = mongoose.model('User', {
-  email: {
-    type: String,
-    trim: true,
-    required: true,
-    minlength: 1
-  }
-});
-
-var User1 = new User({
-  email: 'skynet9054@gmail.com   '
-})
-
-User1.save().then((doc) => {
-  console.log('Email saved successfully', doc);
-}, (e) => {
-  console.log('Err!! : ',e);
-})
+// var User1 = new User({
+//   email: 'skynet9054@gmail.com   '
+// })
+//
+// User1.save().then((doc) => {
+//   console.log('Email saved successfully', doc);
+// }, (e) => {
+//   console.log('Err!! : ',e);
+// })
